c84aa0272d93c6c4fe96e538a9e89f9e41b941f3
Yaku <72257142+YakuDaniro@users.noreply.github.com>
2026-07-06 16:51:02 +0500

    Update index.tsx

--- src/frame/index.tsx
+++ src/frame/index.tsx
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 export function Frame() {
   return (
     <header className={`frame ${styles.frame}`}>
-      <h1 className={styles.frame__title}>С днём рождения,<br>моя любимая Алюня</h1>     
+      <h1 className={styles.frame__title}>С днём рождения,<br />моя любимая Алюня</h1>     
     </header>
   );
 }
